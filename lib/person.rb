f23968eef0a48c2a632faf41f3e2b71ad139afb1
Sergio Robles <sroblesc@gmail.com>
2020-01-08 12:58:40 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,45 @@
 # your code goes here
+
+
+class Person 
+  
+  attr_reader :name
+  attr_accessor :bank_account, :happiness, :hygiene
+  
+  def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+  end
+  
+  
+  def clean?
+    if hygiene > 7 
+      return true
+    end
+  end
+  
+  def happy?
+    if happiness > 7 
+      return true
+    else 
+      return false
+    end
+  end
+    
+    def get_paid(salary)
+      salary + bank_account
+      return "all about the benjamins."
+    end
+    
+    def take_bath 
+      hygiene + 4 
+      return "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+  
+    
+    
+  end
+  
+end
